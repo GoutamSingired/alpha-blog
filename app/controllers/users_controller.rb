a99de4fd2d567497aec23634e17cb705a8e0077b
Goutam <goutam.singireddy@gmail.com>
2020-01-19 09:08:10 -0500

    updated column name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "welcome to alpha blog #{@user.user}"
+      flash[:success] = "welcome to alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:user, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_195335) do
+ActiveRecord::Schema.define(version: 2020_01_19_140153) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_01_14_195335) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
