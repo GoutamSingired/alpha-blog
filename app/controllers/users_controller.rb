8add9e9c9e19145c7d4ed6f36f4bc4eef6c13e34
Goutam <goutam.singireddy@gmail.com>
2020-02-05 14:32:06 -0500

    signup page view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "welcome to alpha blog #{@user.username}"
-      redirect_to articles_path
+       session[:user_id] = @user.id
+       flash[:success] = "welcome to alpha blog #{@user.username}"
+       redirect_to user_path(@user)
     else
       render 'new'
     end
