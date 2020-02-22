a1efa28cdbd093b4308c0a210df895013beabf6d
Goutam <goutam.singireddy@gmail.com>
2020-02-22 10:11:37 -0500

    Article and Category many to many associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_121816) do
+ActiveRecord::Schema.define(version: 2020_02_22_143758) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
