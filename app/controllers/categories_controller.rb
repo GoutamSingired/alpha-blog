e2b2094bdfe6111e63c126fbf4a9d4ee9563d14d
Goutam <goutam.singireddy@gmail.com>
2020-02-25 06:53:04 -0500

    Update all Views of Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
- 
+
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -21,7 +21,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
-
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page:5 )
   end
 
   private
